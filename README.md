6c226f9492cbc956c2374aeca88b6df3ec43efc3
Sanchit Gandhi <sanchit@huggingface.co>
2022-12-07 19:35:33 +0000

    Duplicate from whisper-event/whisper-demo
    Co-authored-by: Sanchit Gandhi <sanchit-gandhi@users.noreply.huggingface.co>

---
+++ README.md
@@ -0,0 +1,15 @@
+---
+title: Whisper Demo
+emoji: 🤫
+colorFrom: indigo
+colorTo: red
+sdk: gradio
+sdk_version: 3.9.1
+app_file: app.py
+pinned: false
+tags:
+- whisper-event
+duplicated_from: whisper-event/whisper-demo
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,98 @@
+import torch
+
+import gradio as gr
+import pytube as pt
+from transformers import pipeline
+from huggingface_hub import model_info
+
+MODEL_NAME = "openai/whisper-small" #this always needs to stay in line 8 :D sorry for the hackiness
+lang = "en"
+
+device = 0 if torch.cuda.is_available() else "cpu"
+
+pipe = pipeline(
+    task="automatic-speech-recognition",
+    model=MODEL_NAME,
+    chunk_length_s=30,
+    device=device,
+)
+
+pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(language=lang, task="transcribe")
+
+def transcribe(microphone, file_upload):
+    warn_output = ""
+    if (microphone is not None) and (file_upload is not None):
+        warn_output = (
+            "WARNING: You've uploaded an audio file and used the microphone. "
+            "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
+        )
+
+    elif (microphone is None) and (file_upload is None):
+        return "ERROR: You have to either use the microphone or upload an audio file"
+
+    file = microphone if microphone is not None else file_upload
+
+    text = pipe(file)["text"]
+
+    return warn_output + text
+
+
+def _return_yt_html_embed(yt_url):
+    video_id = yt_url.split("?v=")[-1]
+    HTML_str = (
+        f'<center> <iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe>'
+        " </center>"
+    )
+    return HTML_str
+
+
+def yt_transcribe(yt_url):
+    yt = pt.YouTube(yt_url)
+    html_embed_str = _return_yt_html_embed(yt_url)
+    stream = yt.streams.filter(only_audio=True)[0]
+    stream.download(filename="audio.mp3")
+
+    text = pipe("audio.mp3")["text"]
+
+    return html_embed_str, text
+
+
+demo = gr.Blocks()
+
+mf_transcribe = gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.inputs.Audio(source="microphone", type="filepath", optional=True),
+        gr.inputs.Audio(source="upload", type="filepath", optional=True),
+    ],
+    outputs="text",
+    layout="horizontal",
+    theme="huggingface",
+    title="Whisper Demo: Transcribe Audio",
+    description=(
+        "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the the fine-tuned"
+        f" checkpoint [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and 🤗 Transformers to transcribe audio files"
+        " of arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+yt_transcribe = gr.Interface(
+    fn=yt_transcribe,
+    inputs=[gr.inputs.Textbox(lines=1, placeholder="Paste the URL to a YouTube video here", label="YouTube URL")],
+    outputs=["html", "text"],
+    layout="horizontal",
+    theme="huggingface",
+    title="Whisper Demo: Transcribe YouTube",
+    description=(
+        "Transcribe long-form YouTube videos with the click of a button! Demo uses the the fine-tuned checkpoint:"
+        f" [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and 🤗 Transformers to transcribe audio files of"
+        " arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+with demo:
+    gr.TabbedInterface([mf_transcribe, yt_transcribe], ["Transcribe Audio", "Transcribe YouTube"])
+
+demo.launch(enable_queue=True)
