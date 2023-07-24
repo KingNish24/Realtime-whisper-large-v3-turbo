3c0cd8eaa310e2659c11207ae51e130a822c1d5b
Sanchit Gandhi <sanchit-gandhi@users.noreply.huggingface.co>
2023-07-24 16:34:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,28 +23,12 @@ pipe = pipeline(
 )
 
 
-def transcribe(microphone, file_upload, task):
-    warn_output = ""
-    if (microphone is not None) and (file_upload is not None):
-        warn_output = (
-            "WARNING: You've uploaded an audio file and used the microphone. "
-            "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
-        )
+def transcribe(inputs, task):
+    if inputs is None:
+        raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
 
-    elif (microphone is None) and (file_upload is None):
-        raise gr.Error("You have to either use the microphone or upload an audio file")
-
-    file_size_mb = os.stat(inputs).st_size / (1024 * 1024)
-    if file_size_mb > FILE_LIMIT_MB:
-        raise gr.Error(
-                f"File size exceeds file size limit. Got file of size {file_size_mb:.2f}MB for a limit of {FILE_LIMIT_MB}MB."
-        )
-
-    file = microphone if microphone is not None else file_upload
-
-    text = pipe(file, batch_size=BATCH_SIZE, generate_kwargs={"task": task})["text"]
-
-    return warn_output + text
+    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
+    return  text
 
 
 def _return_yt_html_embed(yt_url):
@@ -57,23 +41,29 @@ def _return_yt_html_embed(yt_url):
 
 def download_yt_audio(yt_url, filename):
     info_loader = youtube_dl.YoutubeDL()
+    
     try:
         info = info_loader.extract_info(yt_url, download=False)
     except youtube_dl.utils.DownloadError as err:
         raise gr.Error(str(err))
+    
     file_length = info["duration_string"]
     file_h_m_s = file_length.split(":")
     file_h_m_s = [int(sub_length) for sub_length in file_h_m_s]
+    
     if len(file_h_m_s) == 1:
         file_h_m_s.insert(0, 0)
     if len(file_h_m_s) == 2:
         file_h_m_s.insert(0, 0)
     file_length_s = file_h_m_s[0] * 3600 + file_h_m_s[1] * 60 + file_h_m_s[2]
+    
     if file_length_s > YT_LENGTH_LIMIT_S:
         yt_length_limit_hms = time.strftime("%HH:%MM:%SS", time.gmtime(YT_LENGTH_LIMIT_S))
         file_length_hms = time.strftime("%HH:%MM:%SS", time.gmtime(file_length_s))
         raise gr.Error(f"Maximum YouTube length is {yt_length_limit_hms}, got {file_length_hms} YouTube video.")
+    
     ydl_opts = {"outtmpl": filename, "format": "worstvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"}
+    
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.download([yt_url])
@@ -93,7 +83,7 @@ def yt_transcribe(yt_url, task, max_filesize=75.0):
     inputs = ffmpeg_read(inputs, pipe.feature_extractor.sampling_rate)
     inputs = {"array": inputs, "sampling_rate": pipe.feature_extractor.sampling_rate}
 
-    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task})["text"]
+    text = pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
 
     return html_embed_str, text
 
@@ -104,7 +94,24 @@ mf_transcribe = gr.Interface(
     fn=transcribe,
     inputs=[
         gr.inputs.Audio(source="microphone", type="filepath", optional=True),
-        gr.inputs.Audio(source="upload", type="filepath", optional=True),
+        gr.inputs.Radio(["transcribe", "translate"], label="Task", default="transcribe"),
+    ],
+    outputs="text",
+    layout="horizontal",
+    theme="huggingface",
+    title="Whisper Large V2: Transcribe Audio",
+    description=(
+        "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the"
+        f" checkpoint [{MODEL_NAME}](https://huggingface.co/{MODEL_NAME}) and 🤗 Transformers to transcribe audio files"
+        " of arbitrary length."
+    ),
+    allow_flagging="never",
+)
+
+file_transcribe = gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.inputs.Audio(source="upload", type="filepath", optional=True, label="Audio file"),
         gr.inputs.Radio(["transcribe", "translate"], label="Task", default="transcribe"),
     ],
     outputs="text",
@@ -138,7 +145,7 @@ yt_transcribe = gr.Interface(
 )
 
 with demo:
-    gr.TabbedInterface([mf_transcribe, yt_transcribe], ["Transcribe Audio", "Transcribe YouTube"])
+    gr.TabbedInterface([mf_transcribe, file_transcribe, yt_transcribe], ["Microphone", "Audio file", "YouTube"])
 
 demo.launch(enable_queue=True)
 
